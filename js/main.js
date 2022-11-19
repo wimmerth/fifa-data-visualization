eb90341dae2de503202504d7e2152ed4cd012418
Thomas Wimmer <wimmerth@in.tum.de>
2022-11-19 13:10:12 +0100

    Added initial data loading functionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+
+
 const ctx = {
     YEAR : 2015,
 }
 
 function updateYear(input){
-    ctx.YEAR = ctx.YEAR + input;
-    d3.select("#yearLabel").text(ctx.YEAR);
-    console.log("Year changed to " + ctx.YEAR);
+    if (ctx.YEAR + input >= 2015 && ctx.YEAR + input <= 2022){
+        ctx.YEAR = ctx.YEAR + input;
+        d3.select("#yearLabel").text(ctx.YEAR);
+        console.log("Number of players in " + ctx.YEAR + ": " + ctx.playersPerYear[ctx.YEAR].length);
+    } else {
+        console.log("Year out of range");
+    }
 }
 
 function createViz(){
@@ -13,12 +19,30 @@ function createViz(){
     let svg = d3.select("#main").append("svg");
     svg.attr("width", 500);
     svg.attr("height", 500);
+    var rootG = svg.append("g").attr("id", "rootG");
+    rootG.append("g").attr("id","bkgG");
+    rootG.append("g").attr("id","footballfieldG");
+    rootG.append("g").attr("id","comparisonG");
+    rootG.append("g").attr("id","statsG");
     loadData(svg);
 }
 
 function loadData(svg){
-    d3.csv("fifa_players_15_22.csv", (data) => {
-        // do something
+    d3.csv("fifa_players_15_22.csv").then((data) => {
+        console.log("Number of rows: " + data.length);
+        let playersPerYear = {};
+        for (let i = 0; i < data.length; i++){
+            let row = data[i];
+            let year = row["year"];
+            if (year in playersPerYear){
+                playersPerYear[year].push(row);
+            } else {
+                playersPerYear[year] = [row];
+            }
+        }
+        console.log("Number of years: " + Object.keys(playersPerYear).length);
+        console.log("Number of players in " + ctx.YEAR + ": " + playersPerYear[ctx.YEAR].length);
+        ctx.playersPerYear = playersPerYear;
     }).catch((error) => {
         console.log(error);
     });
