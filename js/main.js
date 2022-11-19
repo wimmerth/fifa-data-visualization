fd5ace0513a8888703a8421308535fa8061c8f74
Thomas Wimmer <wimmerth@in.tum.de>
2022-11-19 19:25:39 +0100

    Added a football field visual

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-
-
 const ctx = {
     YEAR : 2015,
+    width : 1900,
+    height : 1900,
+    footballFieldLineWidth : 0.2,
+    backgroundGrey : "#2b2b2b"
 }
 
 function updateYear(input){
@@ -17,14 +19,24 @@ function updateYear(input){
 function createViz(){
     console.log("Using D3 v" + d3.version);
     let svg = d3.select("#main").append("svg");
-    svg.attr("width", 500);
-    svg.attr("height", 500);
+    svg.attr("width", ctx.width);
+    svg.attr("height", ctx.height);
     var rootG = svg.append("g").attr("id", "rootG");
     rootG.append("g").attr("id","bkgG");
     rootG.append("g").attr("id","footballfieldG");
     rootG.append("g").attr("id","comparisonG");
     rootG.append("g").attr("id","statsG");
-    loadData(svg);
+
+    let background = d3.select("#bkgG");
+    background.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", ctx.width)
+        .attr("height", ctx.height)
+        .attr("fill", ctx.backgroundGrey);
+
+    createFootballField();
+    //loadData(svg);
 }
 
 function loadData(svg){
@@ -48,3 +60,131 @@ function loadData(svg){
     });
 }
 
+function createFootballField(){
+    let footballfieldG = d3.select("#footballfieldG");
+    footballfieldG.attr("transform", "translate(100, 100)");
+    ctx.footballFieldScaleX = d3.scaleLinear()
+        .domain([0, 60])
+        .range([0, 600]);
+    ctx.footballFieldScaleY = d3.scaleLinear()
+        .domain([0, 90])
+        .range([0, 900]);
+    let footballfield = footballfieldG.append("g").attr("id", "footballfield");
+    
+    footballfield.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", ctx.footballFieldScaleX(60))
+        .attr("height", ctx.footballFieldScaleY(90))
+        .attr("fill", "white");
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX(ctx.footballFieldLineWidth))
+        .attr("y", ctx.footballFieldScaleY(ctx.footballFieldLineWidth))
+        .attr("width", ctx.footballFieldScaleX(60 - 2 * ctx.footballFieldLineWidth))
+        .attr("height", ctx.footballFieldScaleY(90 - 2 * ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(30))
+        .attr("cy", ctx.footballFieldScaleY(11))
+        .attr("r", ctx.footballFieldScaleX(9.15))
+        .attr("fill", "white");
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(30))
+        .attr("cy", ctx.footballFieldScaleY(79))
+        .attr("r", ctx.footballFieldScaleX(9.15))
+        .attr("fill", "white");
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(30))
+        .attr("cy", ctx.footballFieldScaleY(11))
+        .attr("r", ctx.footballFieldScaleX(9.15 - ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(30))
+        .attr("cy", ctx.footballFieldScaleY(79))
+        .attr("r", ctx.footballFieldScaleX(9.15 - ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+    
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 40.32) / 2))
+        .attr("y", ctx.footballFieldScaleY(0))
+        .attr("width", ctx.footballFieldScaleX(40.32))
+        .attr("height", ctx.footballFieldScaleY(16.5))
+        .attr("fill", "white");
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 40.32) / 2 + ctx.footballFieldLineWidth))
+        .attr("y", ctx.footballFieldScaleY(ctx.footballFieldLineWidth))
+        .attr("width", ctx.footballFieldScaleX(40.32 - 2 * ctx.footballFieldLineWidth))
+        .attr("height", ctx.footballFieldScaleY(16.5 - 2 * ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 40.32) / 2))
+        .attr("y", ctx.footballFieldScaleY(90 - 16.5))
+        .attr("width", ctx.footballFieldScaleX(40.32))
+        .attr("height", ctx.footballFieldScaleY(16.5))
+        .attr("fill", "white");
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 40.32) / 2 + ctx.footballFieldLineWidth))
+        .attr("y", ctx.footballFieldScaleY(90 - 16.5 + ctx.footballFieldLineWidth))
+        .attr("width", ctx.footballFieldScaleX(40.32 - 2 * ctx.footballFieldLineWidth))
+        .attr("height", ctx.footballFieldScaleY(16.5 - 2 * ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 18.32) / 2))
+        .attr("y", ctx.footballFieldScaleY(0))
+        .attr("width", ctx.footballFieldScaleX(18.32))
+        .attr("height", ctx.footballFieldScaleY(5.5))
+        .attr("fill", "white");
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 18.32) / 2 + ctx.footballFieldLineWidth))
+        .attr("y", ctx.footballFieldScaleY(ctx.footballFieldLineWidth))
+        .attr("width", ctx.footballFieldScaleX(18.32 - 2 * ctx.footballFieldLineWidth))
+        .attr("height", ctx.footballFieldScaleY(5.5 - 2 * ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 18.32) / 2))
+        .attr("y", ctx.footballFieldScaleY(90 - 5.5))
+        .attr("width", ctx.footballFieldScaleX(18.32))
+        .attr("height", ctx.footballFieldScaleY(5.5))
+        .attr("fill", "white");
+    footballfield.append("rect")
+        .attr("x", ctx.footballFieldScaleX((60 - 18.32) / 2 + ctx.footballFieldLineWidth))
+        .attr("y", ctx.footballFieldScaleY(90 - 5.5 + ctx.footballFieldLineWidth))
+        .attr("width", ctx.footballFieldScaleX(18.32 - 2 * ctx.footballFieldLineWidth))
+        .attr("height", ctx.footballFieldScaleY(5.5 - 2 * ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+    
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(30))
+        .attr("cy", ctx.footballFieldScaleY(11))
+        .attr("r", ctx.footballFieldScaleX(2 * ctx.footballFieldLineWidth))
+        .attr("fill", "white");
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(30))
+        .attr("cy", ctx.footballFieldScaleY(79))
+        .attr("r", ctx.footballFieldScaleX(2 * ctx.footballFieldLineWidth))
+        .attr("fill", "white");
+
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(30))
+        .attr("cy", ctx.footballFieldScaleY(45))
+        .attr("r", ctx.footballFieldScaleX(9.15))
+        .attr("fill", "white");
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(60 / 2))
+        .attr("cy", ctx.footballFieldScaleY(90 / 2))
+        .attr("r", ctx.footballFieldScaleX(9.15 - ctx.footballFieldLineWidth))
+        .attr("fill", "green");
+    footballfield.append("circle")
+        .attr("cx", ctx.footballFieldScaleX(60 / 2))
+        .attr("cy", ctx.footballFieldScaleY(90 / 2))
+        .attr("r", ctx.footballFieldScaleX(2 * ctx.footballFieldLineWidth))
+        .attr("fill", "white");
+    footballfield.append("rect")
+        .attr("x", 0)
+        .attr("y", ctx.footballFieldScaleY(45-ctx.footballFieldLineWidth / 2))
+        .attr("width", ctx.footballFieldScaleX(60))
+        .attr("height", ctx.footballFieldScaleY(ctx.footballFieldLineWidth))
+        .attr("fill", "white");
+}
