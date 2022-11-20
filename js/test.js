e50f6d223372a77d84b291d5323b73c2cecb2a57
Haileleul Haile <haile.zeyede@gmail.com>
2022-11-20 15:45:30 +0100

    new player postion encoding

--- js/test.js
+++
@@ -1,6 +0,0 @@
-let player_positions = ["LM"];
-let selectedPositions = ["lm", "lam"]
-// let array = JSON.parse("[" + string + "]");
-
-
-console.log(player_positions.some(position => selectedPositions.includes(position.toLowerCase())));
