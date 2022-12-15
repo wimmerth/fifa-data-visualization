db0660ec72377e061034c82842b7f3bee1001249
Haileleul <haile.zeyede@gmail.com>
2022-12-15 18:33:40 +0100

    changed colors for hues

--- js/main.js
+++ js/main.js
@@ -2149,12 +2149,22 @@ function initVariableScatterPlot(g) {
         .domain(generalStatsCTX.relevantPlayers.map(d => d[generalStatsCTX.attrHue]));
     
     if (generalStatsCTX.attrHue == "position") {
-        hueScale = d3.scaleOrdinal(d3.schemeCategory10)
-            .domain(["Defense", "Midfield", "Attack"]);
+            // hueScale = d3.scaleOrdinal(d3.schemeCategory10)
+            //     .domain(["Defense", "Midfield", "Attack"]);
+        hueScale = d3.scaleOrdinal()
+            .domain(["Defense", "Midfield", "Attack"])
+            .range(["#E04371", "#E0D216", "#4DD0F7"]);
+            // .range(["red", "white", "blue"]);
+    }
+    else if (generalStatsCTX.attrHue == "preferred_foot") {
+        hueScale = d3.scaleOrdinal()
+            .range(["#E04371", "#4DD0F7"]);
+            // .range(["red", "white", "blue"]);
     } else if (generalStatsCTX.attrHue == "weak_foot" || generalStatsCTX.attrHue == "skill_moves") {
         hueScale = d3.scaleLinear()
             .domain([2, 3.5, 5])
-            .range(["red", "white", "blue"]);
+            .range(["#B4CBE0", "#70AAE0", "#2D8AE0"]);
+            // .range(["red", "white", "blue"]);
     }
 
     let xAxisG = g.append("g")
@@ -2360,12 +2370,22 @@ function updateVariableScatterPlot(attribute) {
             .range(d3.schemeCategory10);
         
         if (generalStatsCTX.attrHue == "position") {
-            hueScale = d3.scaleOrdinal(d3.schemeCategory10)
-                .domain(["Defense", "Midfield", "Attack"]);
+            // hueScale = d3.scaleOrdinal(d3.schemeCategory10)
+            //     .domain(["Defense", "Midfield", "Attack"]);
+            hueScale = d3.scaleOrdinal()
+                .domain(["Defense", "Midfield", "Attack"])
+                .range(["#E04371", "#E0D216", "#4DD0F7"]);
+                // .range(["red", "white", "blue"]);
+        }
+        else if (generalStatsCTX.attrHue == "preferred_foot") {
+            hueScale = d3.scaleOrdinal()
+                .range(["#E04371", "#4DD0F7"]);
+                // .range(["red", "white", "blue"]);
         } else if (generalStatsCTX.attrHue == "weak_foot" || generalStatsCTX.attrHue == "skill_moves") {
             hueScale = d3.scaleLinear()
                 .domain([2, 3.5, 5])
-                .range(["red", "white", "blue"]);
+                .range(["#B4CBE0", "#70AAE0", "#2D8AE0"]);
+                // .range(["red", "white", "blue"]);
         }
 
         scatterPlotG.selectAll("circle")
